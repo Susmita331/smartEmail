b53bca7ac710bf5c870cf3b56415ee74b5efb0f8
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-26 17:57:06 -0600

    Set up Gemini API config in EmailGeneratorService and start building request payload

--- src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,17 +1,38 @@
 package com.email.writer.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class EmailGeneratorService {
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
     public String generateEmailReply(EmailRequest emailRequest) {
+
         //Build the prompt
-        String prmpt = buildPrompt(emailRequest)
+        String prompt = buildPrompt(emailRequest);
         //Craft the request
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of("parts", new Object[]{
+                                Map.of("text", prompt)
+                        })
+                }
+        );
     }
     private String buildPrompt(EmailRequest emailRequest) {
         StringBuilder prompt = new StringBuilder();
         prompt.append("Generate a professional email reply for the following email content");
-        return null;
+        if (emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()) {
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+
     }
 }
